001dca18a3edd7e7336f3cd98b3e9bda6d68b43b
Ila-bura <ila.bura@gmail.com>
2021-05-23 14:36:21 +0100

    Updated css

--- src/Weather.js
+++ src/Weather.js
@@ -3,30 +3,10 @@ import "./Weather.css";
 
 
 export default function Weather() {
-  let weatherData = {
-    city: "Dublin",
-    date: "Sunday 9 May",
-    description: "Clear Sky",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
-    humidity: 80,
-    wind: 10,
-    temperature: 12
-  };
+  
 
   return (
     <div className="Weather">
-      <div className="date_form">
-        <div className="row">
-          <div className="col-6">
-            <div className="date">
-              <h1>
-                <span img="emoji" description="aria-label"></span>
-                {"📆 "}
-                {weatherData.date}
-              </h1>
-            </div>
-          </div>
-          <div className="col-6">
             <form className="search_form">
               <input
                 type="search"
@@ -41,42 +21,38 @@ export default function Weather() {
                 className="search_button"
               />
             </form>
+        
+      
+     
+      <h1>Vienna</h1>
+      <ul>
+        <li>Wednesady 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+      <div className="clearfix">
+          <img 
+          src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" 
+          alt="Mostly cloudy"
+          className="float-left" 
+          />
+     <div className="float-left">
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
           </div>
+          </div>
+             </div>
+        <div className="col-6">
+          <ul>
+        <li>Precipitation: 15%</li>
+        <li>Humidity: 72%</li>
+         <li>Wind: 13km/h</li>
+      </ul>
         </div>
       </div>
-      <div className="city_name">
-        <div className="row">
-          <div className="col-md-auto">
-            <h2>
-              {weatherData.city}
-              <img
-                src={weatherData.imgUrl}
-                alt={weatherData.description}
-                className="forecast-icon"
-              />
-            </h2>
-          </div>
-        </div>
-      </div>
-      <section className="current_weather">
-        <div>
-          <span className="current_weather_icon">
-            {weatherData.description}
-          </span>
-          <span className="temp_unit">
-            <strong>
-              <span className="temperature">{weatherData.temperature}</span>
-            </strong>
-            <span className="units">
-              <a href="/">°C</a> | <a href="/">°F</a>
-            </span>
-          </span>
-        </div>
-        <span className="humidity_wind">
-          <li>Humidity: {weatherData.humidity}%</li>
-          <li>Wind: {weatherData.wind} km/h</li>
-        </span>
-      </section>
+
+      
 
       <p className="footer">
         <a
