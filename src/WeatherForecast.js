2861249908c3726dd154683abdf32cc5d04b19b9
Ila-bura <ila.bura@gmail.com>
2021-06-04 10:47:06 +0100

    Integrated refresh feature, removed °F link, styled weather row

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
+
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+    setLoaded(false);   
+
+    }, [props.coordinates]);
 
 function handleResponse(response) {
 setForecast(response.data.daily);
@@ -20,15 +26,14 @@ return (
     if (index < 5) {
 return (
 <div className="col" key={index}>
-            <WeatherForecastDay data={dailyForecast} /> 
-            </div>
+ <WeatherForecastDay data={dailyForecast} /> 
+</div>
 );
-    }
+}
 })}
-
-    </div>
-    </div>
-    );
+</div>
+</div>
+);
 }
 else {
     let apiKey = "f0598073f30911a6c5752b5952f5caea";
